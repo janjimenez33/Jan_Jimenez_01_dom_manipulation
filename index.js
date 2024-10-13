1b49567dc132a8531282c5013f74d5f6bb0290d9
Jan <jan.jimenez@mataro.epiaedu.cat>
2024-10-13 18:53:24 +0200

    Exercises 5, 6, 7, 8, 9 and 10

--- index.js
+++ index.js
@@ -16,4 +16,49 @@ setTimeout(() => {
 2);
 
 //EXERCISE 5
+let button = document.createElement('button');
 
+button.textContent = 'Click Me';
+
+document.getElementById('content').appendChild(button);
+
+//EXERCISE 6
+let resultElement = document.getElementById('result');
+
+if (resultElement) {
+    resultElement.remove();
+}
+
+//EXERCISE 7
+button.addEventListener('click', () => {
+    alert('Button Clicked!');
+});
+
+//EXERCISE 8
+let inputField = document.querySelector('input');
+let newResultDiv = document.createElement('div');
+document.body.appendChild(newResultDiv);
+
+inputField.addEventListener('input', () => {
+    newResultDiv.textContent = inputField.value;
+});
+
+//EXERCISE 9
+let actionButton = document.getElementById('actionButton');
+let contentDiv = document.getElementById('content');
+
+actionButton.addEventListener('click', () => {
+    if (contentDiv.style.display === 'none') {
+        contentDiv.style.display = 'block';
+    } else {
+        contentDiv.style.display = 'none';
+    }
+});
+
+//EXERCISE 10
+let inputFieldName = document.getElementById('inputField');
+if (inputFieldName) {
+    inputField.placeholder = "Enter your name";
+} else {
+    console.error('Input field not found');
+}
